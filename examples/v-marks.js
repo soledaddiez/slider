64b60569f0f68d5a4f350098879a94288078b9fb
Soledad Diez <diez.soledad@gmail.com>
2016-08-22 16:48:19 +1200

    Updating package name for npm repo

--- examples/v-marks.js
+++ examples/v-marks.js
@@ -1,8 +1,8 @@
-require('rc-slider/assets/index.less');
+require('rc-slider-colored/assets/index.less');
 
 const React = require('react');
 const ReactDOM = require('react-dom');
-const Slider = require('rc-slider');
+const Slider = require('rc-slider-colored');
 
 const style = { float: 'left', width: 160, height: 400, marginBottom: 160, marginLeft: 50 };
 const parentStyle = { overflow: 'hidden' };
